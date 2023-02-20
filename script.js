691846db96c505f191e6d167ebfc7afc960f1510
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-02-20 13:53:26 -0500

    XD

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // i nicio 
+let ronda = 0;
 
 alert("seleccione dificultad");
 
@@ -14,9 +15,9 @@ const btnnormal = () => {
     let bot1 = Math.round(Math.random(0,1,2)*2);
     //console.log (bot1);
 
-    // player
+    // Player //
     do {
-        player = prompt(" escribe 0 para piedra, 1 para papel, 2 para digeras ");
+        player = prompt(" escribe 0 para piedra, 1 para papel, 2 para tijeras ");
     } while (player < 0 || player >= 3);
 
     if (player == 0) {
@@ -24,7 +25,7 @@ const btnnormal = () => {
     } else if (player == 1) {
         alert(" jugaste papel ");
     } else if (player == 2) {
-        alert(" jugaste tigeras ");
+        alert(" jugaste tijeras ");
     };
 
     // bot1 
@@ -34,13 +35,13 @@ const btnnormal = () => {
     } else if (bot1 == 1) {
         alert(" bot1 jugo papel ");
     } else if (bot1 == 2) {
-        alert(" bot1 jugo tigeras ");
+        alert(" bot1 jugo tijeras ");
     };
 
-    //validar
+    // validar //
 
     if (player == bot1) {
-        alert(" Empate pirobo ")
+        alert(" Empate ")
         btnnormal ();
     } else if (player == 0 && bot1 == 2) {
         alert(" felicidades gano ");
@@ -54,12 +55,77 @@ const btnnormal = () => {
     };
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Welcome Hardmode //
+
+const btwhardmode = () => {
+    let player = 0;
+    let Hard = 0;
+    ronda += 1;
+    console.log (ronda)
+    // player
+    do {
+        player = prompt(" escribe 0 para piedra, 1 para papel, 2 para tijeras ");
+    } while (player < 0 || player >= 3);
+
+    if (player == 0) {
+        alert(" Jugaste piedra ");
+    } else if (player == 1) {
+        alert(" jugaste papel ");
+    } else if (player == 2) {
+        alert(" jugaste tijeras ");
+    };
+    // hard //
+    if (player == 0) {
+        Hard = 1
+        alert(" script jugo papel ")
+    } else if (player == 1) {
+        Hard = 2
+        alert(" script jugo tijeras ")
+    } else if (player == 2) {
+        Hard = 0
+        alert(" script jugo piedra ")
+    }
+    /*
+    // Bot1 //
+    
+    if (bot1 == 0) {
+        alert(" bot1 jugo piedra ");
+    } else if (bot1 == 1) {
+        alert(" bot1 jugo papel ");
+    } else if (bot1 == 2) {
+        alert(" bot1 jugo tijeras ");
+    };
+    */
+
+    //validar
+
+    if (player == Hard) {
+        alert(" Empate ")
+        btwhardmode ();
+    } else if (player == 0 && Hard == 2) {
+        alert(" felicidades gano ");
+    } else if (player == 1 && Hard == 0) {
+        alert(" felicidades gano ");
+    } else if (player == 2 && Hard == 1) {
+        alert(" felicidades gano ");
+    } else if (ronda == 3) {
+        alert(" perdiste vete al modo normal ");
+        console.log(ronda)
+        btnnormal (); 
+    } else {
+      alert("perdiste")
+      btwhardmode ();
+    }
+};
+
                             //Lista de cosas por soluccionar o agregar//
 // 01 agregar dificultad dificil y facil 
 // 02 pixel art ( ni idea como voy hacer esta weba);
 // 03 crear tabla de puntos con nombres 
 // 04 base de datos para guardar puntages
-// 05 agregar login y registro para complementar sistema de puntages 
+// 05 agregar login y registro para complementar sistema de puntajes 
 // 06 hacer multijugador local 
 // pagina del index pagina funcionalidad del login / registro 
 // colocar insultos a moises 
